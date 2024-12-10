6c42b73c3877d6cdc1b1ad2ae91f1b6a8db897a0
dofxo <kargat504@gmail.com>
2024-12-10 23:49:53 +0330

    change some styles

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,10 +1,11 @@
+import { MainContext } from "@/context";
 import { skills } from "@/data/skills";
 import { Rocket } from "lucide-react";
 import { useContext } from "react";
-import { MainContext } from "@/context";
 
 const Skills = () => {
   const { theme } = useContext(MainContext);
+  const iconsTextColor = theme === "light" ? "text-gray-900" : "text-gray-300";
   return (
     <section>
       <div className="container flex flex-col gap-10">
@@ -14,9 +15,14 @@ const Skills = () => {
         </h2>
         <div className="skills flex gap-5 flex-wrap">
           {skills.map((skill, idx) => (
-            <div className="flex gap-2 items-center border bg-[var(--badge-bg-color)] px-2.5 py-1.5 rounded-full">
-              <skill.icon color={skill.color} className="align-middle inline" />
-              <span className="text-[12px] text-gray-600">{skill.text}</span>
+            <div
+              key={idx}
+              className="flex gap-2 items-center border bg-[var(--badge-bg-color)] px-2.5 py-1.5 rounded-full"
+            >
+              <skill.icon color={skill.color} />
+              <span className={`text-[12px] ${iconsTextColor}`}>
+                {skill.text}
+              </span>
             </div>
           ))}
         </div>
