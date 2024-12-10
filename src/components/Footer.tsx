904ee0fa99e10907ff3c6485cdbbc609885c02f7
dofxo <kargat504@gmail.com>
2024-12-11 01:30:49 +0330

    change footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { HeartIcon } from "lucide-react";
 
 const Footer = () => {
   return (
-    <footer className="p-5 border-t text-[var(--text-color)]">
+    <footer className="p-5 border-t text-[var(--text-color)] flex justify-center">
       <div className="container">
         <div className="flex justify-between text-[13px]">
           <span className="flex gap-2 items-center">
