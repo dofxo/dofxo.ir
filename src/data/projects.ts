a55f233c6ff3317ceadd37f6ea4e06d7dbfeb40f
dofxo <kargat504@gmail.com>
2025-10-08 15:46:00 +0330

    feat: add new project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,87 +1,102 @@
 import { projectsType } from "@/types";
 
 export const projects: projectsType = [
-  {
-    title: {
-      fa: "SilverBox",
-      en: "SilverBox",
-    },
-    websiteLink: "https://silverboxjs.ir/",
-    description: {
-      fa: `کتابخانه جاوااسکریپت سبک و چندمنظوره برای ایجاد مدال‌ها و اعلان‌های سفارشی برای برنامه‌های وب. ساخته شده با جاوااسکریپت خالص و بدون وابستگی.`,
-      en: `A lightweight and versatile JavaScript library for creating custom modals and alerts for web applications. Built with pure JavaScript and no dependencies.`,
-    },
-    role: "Developer",
-    sourceCode: "https://github.com/silverethical/silverbox/",
-    skills: ["JavaScript", "SCSS"],
-  },
-  {
-    title: {
-      fa: "فرانت چپتر (1403)",
-      en: "Front Chapter (2024)",
-    },
-    websiteLink: "https://frontchapter.ir/",
-    description: {
-      fa: `لندینگ سایت فرانت چپتر برای همایش سال 1403 در شیراز طراحی و توسعه داده شد. به‌عنوان فرانت‌اند دولوپر افتخار داشتم در کنار تیم فنی فرانت چپتر در این پروژه حضور داشته باشم.`,
-      en: `Front Chapter’s landing page for the 2024 conference in Shiraz was designed and developed. I had the honor of contributing as a front-end developer alongside the Front Chapter technical team.`,
-    },
-    role: "FrontEnd Developer",
-    sourceCode: "https://github.com/frontChapter/nextjs",
-    skills: ["Next.js", "TailwindCSS", "Launch UI"],
-  },
-  {
-    title: {
-      fa: "تنرو",
-      en: "Tonrow",
-    },
-    websiteLink: "https://tonrow.ir/",
-    role: "FrontEnd Developer",
-    description: {
-      fa: `اپلیکیشن وب تحویل آنلاین برای شهر بوشهر، ایران که شامل درخواست‌های خرید و انتقال بسته‌ها می‌باشد.`,
-      en: `An online delivery web app for the city of Bushehr, Iran — handling package delivery and shopping requests.`,
-    },
-    skills: ["JavaScript", "SCSS", "EJS", "Socket.io", "Swiper", "Alpine.js"],
-  },
-  {
-    title: {
-      fa: "مربی باشگاه",
-      en: "Gym Trainer",
-    },
-    websiteLink: "https://heydarifatemeh.ir/",
-    sourceCode: "https://github.com/Silverethical/gym-trainer-website/",
-    role: "FrontEnd Developer",
-    description: {
-      fa: `وبسایت مربی باشگاه برای فاطمه حیدری، شامل خدمات وی در حوزه تناسب اندام و سلامتی و اطلاعات کامل درباره خدمات او می‌باشد.`,
-      en: `A fitness trainer website for Fatemeh Heydari, featuring her services in fitness and health, along with detailed information about her programs.`,
-    },
-    skills: ["TypeScript", "React.js", "Swiper", "TailwindCSS"],
-  },
-  {
-    title: {
-      fa: "NSD",
-      en: "NSD",
-    },
-    websiteLink: "https://nirosaiedarakhsh.ir/",
-    sourceCode: "https://github.com/fulcain/niro-saee",
-    role: "FrontEnd Developer",
-    description: {
-      fa: `طراحی و توسعه وب‌سایتی حرفه‌ای برای معرفی خدمات شرکت نیرو ساعی درخش، شامل بخش‌های معرفی شرکت، خدمات مهندسی و پیمانکاری، نمونه‌کارها، و ارتباط با مشتریان. این وب‌سایت با طراحی مدرن و کاربرپسند، نمایانگر هویت و فعالیت‌های شرکت است.`,
-      en: `A professional website designed and developed for Niro Saee Derakhsh company, showcasing its engineering and contracting services, portfolio, and client communication. The site features a modern and user-friendly design that reflects the company's identity and activities.`,
-    },
-    skills: ["TypeScript", "Next.js", "Swiper", "shadcn/ui", "TailwindCSS"],
-  },
-  {
-    title: {
-      fa: "فاکتور فروش رمیونا",
-      en: "Remiona Invoice",
-    },
-    websiteLink: "https://factor.remiona.ir",
-    sourceCode: "https://github.com/Silverethical/online_invoice/",
-    role: "FrontEnd Developer",
-    description: {
-      fa: `این پروژه ابزاری حرفه‌ای برای صدور فاکتور فروشگاهی است که به‌صورت اختصاصی برای فروشگاه رمیونا طراحی و پیاده‌سازی شده است. این ابزار به فروشگاه کمک می‌کند تا فرآیند صدور فاکتور را سریع‌تر، دقیق‌تر و استانداردتر انجام داده و اطلاعات خرید مشتریان را به شکلی منظم ذخیره کند. (پروژه صرفا جهت استفاده در دسکتاپ است)`,
-      en: `A professional invoicing tool specifically designed for Remiona store. It helps streamline the billing process, making it faster, more accurate, and standardized while neatly storing customer purchase information. (Desktop-only project)`,
-    },
-    skills: ["TypeScript", "React.js", "MUI", "TailwindCSS", "Ant Design"],
-  },
+	{
+		title: {
+			fa: "SilverBox",
+			en: "SilverBox",
+		},
+		websiteLink: "https://silverboxjs.ir/",
+		description: {
+			fa: `کتابخانه جاوااسکریپت سبک و چندمنظوره برای ایجاد مدال‌ها و اعلان‌های سفارشی برای برنامه‌های وب. ساخته شده با جاوااسکریپت خالص و بدون وابستگی.`,
+			en: `A lightweight and versatile JavaScript library for creating custom modals and alerts for web applications. Built with pure JavaScript and no dependencies.`,
+		},
+		role: "Developer",
+		sourceCode: "https://github.com/silverethical/silverbox/",
+		skills: ["JavaScript", "SCSS"],
+	},
+	{
+		title: {
+			fa: "فرانت چپتر (1403)",
+			en: "Front Chapter (2024)",
+		},
+		websiteLink: "https://frontchapter.ir/",
+		description: {
+			fa: `لندینگ سایت فرانت چپتر برای همایش سال 1403 در شیراز طراحی و توسعه داده شد. به‌عنوان فرانت‌اند دولوپر افتخار داشتم در کنار تیم فنی فرانت چپتر در این پروژه حضور داشته باشم.`,
+			en: `Front Chapter’s landing page for the 2024 conference in Shiraz was designed and developed. I had the honor of contributing as a front-end developer alongside the Front Chapter technical team.`,
+		},
+		role: "FrontEnd Developer",
+		sourceCode: "https://github.com/frontChapter/nextjs",
+		skills: ["Next.js", "TailwindCSS", "Launch UI"],
+	},
+	{
+		title: {
+			fa: "تنرو",
+			en: "Tonrow",
+		},
+		websiteLink: "https://tonrow.ir/",
+		role: "FrontEnd Developer",
+		description: {
+			fa: `اپلیکیشن وب تحویل آنلاین برای شهر بوشهر، ایران که شامل درخواست‌های خرید و انتقال بسته‌ها می‌باشد.`,
+			en: `An online delivery web app for the city of Bushehr, Iran — handling package delivery and shopping requests.`,
+		},
+		skills: ["JavaScript", "SCSS", "EJS", "Socket.io", "Swiper", "Alpine.js"],
+	},
+	{
+		title: {
+			fa: "مربی باشگاه",
+			en: "Gym Trainer",
+		},
+		websiteLink: "https://heydarifatemeh.ir/",
+		sourceCode: "https://github.com/Silverethical/gym-trainer-website/",
+		role: "FrontEnd Developer",
+		description: {
+			fa: `وبسایت مربی باشگاه برای فاطمه حیدری، شامل خدمات وی در حوزه تناسب اندام و سلامتی و اطلاعات کامل درباره خدمات او می‌باشد.`,
+			en: `A fitness trainer website for Fatemeh Heydari, featuring her services in fitness and health, along with detailed information about her programs.`,
+		},
+		skills: ["TypeScript", "React.js", "Swiper", "TailwindCSS"],
+	},
+	{
+		title: {
+			fa: "NSD",
+			en: "NSD",
+		},
+		websiteLink: "https://nirosaiedarakhsh.ir/",
+		sourceCode: "https://github.com/fulcain/niro-saee",
+		role: "FrontEnd Developer",
+		description: {
+			fa: `طراحی و توسعه وب‌سایتی حرفه‌ای برای معرفی خدمات شرکت نیرو ساعی درخش، شامل بخش‌های معرفی شرکت، خدمات مهندسی و پیمانکاری، نمونه‌کارها، و ارتباط با مشتریان. این وب‌سایت با طراحی مدرن و کاربرپسند، نمایانگر هویت و فعالیت‌های شرکت است.`,
+			en: `A professional website designed and developed for Niro Saee Derakhsh company, showcasing its engineering and contracting services, portfolio, and client communication. The site features a modern and user-friendly design that reflects the company's identity and activities.`,
+		},
+		skills: ["TypeScript", "Next.js", "Swiper", "shadcn/ui", "TailwindCSS"],
+	},
+	{
+		title: {
+			fa: "ECRP Map POI",
+			en: "ECRP Map POI",
+		},
+		websiteLink: "https://dofxo.github.io/ecrp-map-poi/",
+		sourceCode: "https://github.com/dofxo/ecrp-map-poi",
+		role: "FrontEnd Developer",
+		description: {
+			fa: `این پروژه ابزاری برای سرور بازی eclipse roleplay هست که متمرکز برای ذخیره اطلاعات مربوط به یکی از ارگان های درون بازی است.`,
+			en: `This project is a tool for the Eclipse Roleplay game server, focused on storing data related to one of the in-game organizations.
+`,
+		},
+		skills: ["TypeScript", "React.js", "TailwindCSS", "Ant Design", "Supabase"],
+	},
+	{
+		title: {
+			fa: "فاکتور فروش رمیونا",
+			en: "Remiona Invoice",
+		},
+		websiteLink: "https://factor.remiona.ir",
+		sourceCode: "https://github.com/Silverethical/online_invoice/",
+		role: "FrontEnd Developer",
+		description: {
+			fa: `این پروژه ابزاری حرفه‌ای برای صدور فاکتور فروشگاهی است که به‌صورت اختصاصی برای فروشگاه رمیونا طراحی و پیاده‌سازی شده است. این ابزار به فروشگاه کمک می‌کند تا فرآیند صدور فاکتور را سریع‌تر، دقیق‌تر و استانداردتر انجام داده و اطلاعات خرید مشتریان را به شکلی منظم ذخیره کند. (پروژه صرفا جهت استفاده در دسکتاپ است)`,
+			en: `A professional invoicing tool specifically designed for Remiona store. It helps streamline the billing process, making it faster, more accurate, and standardized while neatly storing customer purchase information. (Desktop-only project)`,
+		},
+		skills: ["TypeScript", "React.js", "MUI", "TailwindCSS", "Ant Design"],
+	},
 ];
